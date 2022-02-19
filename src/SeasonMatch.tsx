9f9a0628d74d345c8180e8baa67812ccf991abdf
bpeters <brennen.peters@gmail.com>
2022-02-19 08:58:34 -0700

    updates

--- src/SeasonMatch.tsx
+++ src/SeasonMatch.tsx
@@ -21,10 +21,109 @@ import { FighterPortrait, FighterStats } from './Fighter';
 import { matchReporter } from "./utils/fighting";
 import { getMatch, RemoteChainPayloadContext } from "./utils/firebase";
 
+const Fighters = ({ fighter1, fighter2, fighter1Winner, fighter2Winner}: any) => {
+  return (
+    <HStack marginTop={12} align="flex-start" spacing={8}>
+      <VStack>
+        <FighterPortrait fighter={fighter1} winner={fighter1Winner} />
+        <FighterStats fighter={fighter1} />
+      </VStack>
+      <Box
+        display='flex'
+        height={{ base: "100px", md: 150 }}
+        justifyContent="center"
+        alignItems="center"
+        opacity={0.5}
+      >
+        <RiSwordFill size={24} />
+      </Box>
+      <VStack>
+        <FighterPortrait fighter={fighter2} winner={fighter2Winner} />
+        <FighterStats fighter={fighter2} />
+      </VStack>
+    </HStack>
+  );
+};
+
+const MatchLog = ({ report, fighter1, fighter2 }: any) => {
+  const lineColor = useColorModeValue('gray.500', 'white.500');
+
+  return (
+    <>
+      <Heading size='md' marginTop={12} textAlign="center" lineHeight={1.5} marginBottom={8}>
+        Match Log
+      </Heading>
+      <VStack width="100%" justify="center" align="center" spacing={4}>
+        {report.map((r: any, key: number) => {
+          const player = r.turn === '0' ? fighter1 : fighter2;
+
+          if (r.turn === '0') {
+            return (
+              <HStack
+                key={key}
+                width="100%"
+                justify="flex-start"
+                align="center"
+                borderColor={lineColor}
+                padding={6}
+                spacing={8}
+              >
+                <Center width="40px">
+                  <Image
+                    boxSize={{ base: "32px" }}
+                    borderRadius={{ base: "32px" }}
+                    minWidth="32px"
+                    src={player.image_thumbnail_url}
+                  />
+                </Center>
+                <Center textAlign="left">
+                  <Text fontSize={12} fontWeight={400} color="blue.500">
+                    {_.split(r.log, '|').map((w: any, i: number) => {
+                      return i % 2 ? <span style={{ fontWeight: 900 }} key={i}>{w}</span> : <span key={i}>{w}</span>
+                    })}
+                  </Text>
+                </Center>
+              </HStack>
+            );
+          } else {
+            return (
+              <HStack
+                key={key}
+                width="100%"
+                justify="flex-end"
+                align="center"
+                borderColor={lineColor}
+                padding={6}
+                spacing={8}
+              >
+                <Center textAlign="right">
+                  <Text fontSize={12} fontWeight={400} color="red.500">
+                    {_.split(r.log, '|').map((w: any, i: number) => {
+                      return i % 2 ? <span style={{ fontWeight: 900 }} key={i}>{w}</span> : <span key={i}>{w}</span>
+                    })}
+                  </Text>
+                </Center>
+                <Center width="40px">
+                  <Image
+                    boxSize={{ base: "32px" }}
+                    borderRadius={{ base: "32px" }}
+                    src={player.image_thumbnail_url}
+                    minWidth="32px"
+                  />
+                </Center>
+              </HStack>
+            );
+          }
+        })}
+      </VStack>
+    </>
+  );
+};
+
 export const SeasonMatch = (props: any) => {
   const toast = useToast();
 
-  const LineColor = useColorModeValue('gray.500', 'white.500');
+
   const winnerColor = useColorModeValue('gray.800', 'white');
 
   const matchId = props.id;
@@ -67,8 +166,6 @@ export const SeasonMatch = (props: any) => {
       });
     }
   }, [errorLoading, toast]);
-
-  console.log(match);
 
   const report =  match.log ? matchReporter({
     match: match.log,
@@ -125,94 +222,18 @@ export const SeasonMatch = (props: any) => {
       <Text marginTop={2} fontSize={12} textAlign="center">
         {match.block} {inBlocks > 0 ? `(${inBlocks})` : ''}
       </Text>
-      <HStack marginTop={12} align="flex-start" spacing={8}>
-        <VStack>
-          <FighterPortrait fighter={fighter1} winner={fighter1Winner} />
-          <FighterStats fighter={fighter1} />
-        </VStack>
-        <Box
-          display='flex'
-          height={{ base: "100px", md: 150 }}
-          justifyContent="center"
-          alignItems="center"
-          opacity={0.5}
-        >
-          <RiSwordFill size={24} />
-        </Box>
-        <VStack>
-          <FighterPortrait fighter={fighter2} winner={fighter2Winner} />
-          <FighterStats fighter={fighter2} />
-        </VStack>
-      </HStack>
+      <Fighters
+        fighter1={fighter1}
+        fighter1Winner={fighter1Winner}
+        fighter2={fighter2}
+        fighter2Winner={fighter2Winner}
+      />
       {match.log && (
-        <>
-          <Heading size='md' marginTop={12} textAlign="center" lineHeight={1.5} marginBottom={8}>
-            Match Log
-          </Heading>
-          <VStack width="100%" justify="center" align="center" spacing={4}>
-            {report.map((r: any, key: number) => {
-              const player = r.turn === '0' ? fighter1 : fighter2;
-
-              if (r.turn === '0') {
-                return (
-                  <HStack
-                    key={key}
-                    width="100%"
-                    justify="flex-start"
-                    align="center"
-                    borderColor={LineColor}
-                    padding={6}
-                    spacing={8}
-                  >
-                    <Center width="40px">
-                      <Image
-                        boxSize={{ base: "32px" }}
-                        borderRadius={{ base: "32px" }}
-                        minWidth="32px"
-                        src={player.image_thumbnail_url}
-                      />
-                    </Center>
-                    <Center textAlign="left">
-                      <Text fontSize={12} fontWeight={400} color="blue.500">
-                        {_.split(r.log, '|').map((w: any, i: number) => {
-                          return i % 2 ? <span style={{ fontWeight: 900 }} key={i}>{w}</span> : <span key={i}>{w}</span>
-                        })}
-                      </Text>
-                    </Center>
-                  </HStack>
-                );
-              } else {
-                return (
-                  <HStack
-                    key={key}
-                    width="100%"
-                    justify="flex-end"
-                    align="center"
-                    borderColor={LineColor}
-                    padding={6}
-                    spacing={8}
-                  >
-                    <Center textAlign="right">
-                      <Text fontSize={12} fontWeight={400} color="red.500">
-                        {_.split(r.log, '|').map((w: any, i: number) => {
-                          return i % 2 ? <span style={{ fontWeight: 900 }} key={i}>{w}</span> : <span key={i}>{w}</span>
-                        })}
-                      </Text>
-                    </Center>
-                    <Center width="40px">
-                      <Image
-                        boxSize={{ base: "32px" }}
-                        borderRadius={{ base: "32px" }}
-                        src={player.image_thumbnail_url}
-                        minWidth="32px"
-                      />
-                    </Center>
-                  </HStack>
-                );
-              }
-            })}
-          </VStack>
-        </>
+        <MatchLog
+          report={report}
+          fighter1={fighter1}
+          fighter2={fighter2}
+        />
       )}
       <Button
         marginTop={16}
